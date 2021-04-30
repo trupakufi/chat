1d0cf60be429d264a711e812491634404918de30
mariovarela99@outlook.com <mariovarela99@outlook.com>
2021-04-30 23:53:37 +0100

    :rocket: Parte de Comunication Feita

--- server.js
+++ server.js
@@ -7,9 +7,22 @@ const io = require("socket.io")(server);
 
 app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "public"));
-ap.engine("html", require("ejs").renderFile);
+app.engine("html", require("ejs").renderFile);
 app.set("view engine", "html");
 
-app.use("/", (req, res) => res.render("index.html"));
+app.use("/", (req, res) => {
+  res.render("index.html");
+});
+
+let messages = [];
+
+io.on("connection", (socket) => {
+  console.log(`Socket Conectado =>  ${socket.id}`);
+
+  socket.on("sendMessage", (data) => {
+    console.log(data);
+    messages.push(data);
+  });
+});
 
 server.listen(3000);
